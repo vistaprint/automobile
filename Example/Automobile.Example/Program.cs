8cff8aeffcb74787c231473cd1314af96dd13209
dstiert <dstiert@vistaprint.com>
2013-07-26 14:54:34 -0400

    Bare bones example project

---
+++ Example/Automobile.Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using Automobile.Mobile.Framework;
+using Automobile.Mobile.Framework.Data;
+
+namespace Automobile.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MobileDb.CreateRegistrarClient("http://localhost:8080", new JsonProvider());
+
+            var device = new DeviceInfo {UniqueId = "id"};
+            MobileDb.Instance.Register(device);
+
+            MobileDb.Instance.SetAvailibility(device, false);
+        }
+    }
+}
